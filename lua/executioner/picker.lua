379637d6e48af8c55a6cc9d64e1a5abad0b94271
Gabriel Fernandes <gfe@certi.org.br>
2026-04-09 09:18:48 -0300

    add rockspec and tags

--- lua/executioner/picker.lua
+++ lua/executioner/picker.lua
@@ -9,7 +9,6 @@ local finders = require 'telescope.finders'
 local conf = require('telescope.config').values
 local actions = require 'telescope.actions'
 local action_state = require 'telescope.actions.state'
-local previewers = require 'telescope.previewers'
 local themes = require 'telescope.themes'
 
 local M = {}
